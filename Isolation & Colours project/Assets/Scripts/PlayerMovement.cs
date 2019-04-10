d23e376d0955b4fccad4718450c621a26c2db5d0
Soren's Coding Land <chaz.com@hotmail.co.uk>
2019-04-10 23:20:19 +0100

    Rotation - In progress
    I was unable to get the ship rotating without issues so I've opted to rotate the cannon but the cannon doesn't fully rotate the way that I want it to so further work must be done.

--- Isolation & Colours project/Assets/Scripts/PlayerMovement.cs
+++ Isolation & Colours project/Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] GameObject[] guns;
 
     Rigidbody2D rb2D;
+    float rotation = 0f; // It may need a more specific name.
 
     void Awake()
     {
@@ -51,16 +52,42 @@ public class PlayerMovement : MonoBehaviour
             //rb2D.velocity = transform.up * Thrustspeed;
             //transform.Translate(Vector2.up * Thrustspeed * Time.deltaTime);
         }
-        //else if (Input.GetKey(KeyCode.Q))
+        //else if (Input.Getkey(keycode.q))
         //{
-        //    transform.Rotate(transform.forward * Rotationspeed * Time.deltaTime);
-        //}
-        //if (Input.GetKey(KeyCode.E))
-        //{
-        //    transform.Rotate(-transform.forward * Rotationspeed * Time.deltaTime);
+        //    rotation -= rotationspeed * time.deltatime;
+        
+        //if (input.getkey(keycode.e))
+
+        //    rotation += rotationspeed * time.deltatime;
+
+        //    rotation = mathf.clamp(rotation, -90f, 90f);
+        //    var clampedrotation = transform.rotation; // this a local quaternion rotation that the transform.rotation is equalling.
+        //    clampedrotation.z = rotation; // do i use y, x or z?
+        //    transform.rotation = clampedrotation; // ?
         //}
 
         ProcessFiring();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Find("Paint Cannon Body").Rotate(new Vector3(0, 0, 1) * Rotationspeed * Time.deltaTime, Space.Self);
+            //transform.Rotate(Vector3.forward, -Rotationspeed * Time.deltaTime); // Is forward using local or world rotation?
+            //rotation -= Rotationspeed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.E))
+        {
+            transform.Find("Paint Cannon Body").Rotate(new Vector3(0, 0, -1) * Rotationspeed * Time.deltaTime, Space.Self);
+            //rotation += Rotationspeed * Time.deltaTime;
+        }
+
+        float minRotation = -45;
+        float maxRotation = 45;
+        Vector3 currentRotation = transform.Find("Paint Cannon Body").localRotation.eulerAngles;
+        currentRotation.z = Mathf.Clamp(currentRotation.z, minRotation, maxRotation);
+        transform.Find("Paint Cannon Body").localRotation = Quaternion.Euler(currentRotation);
+        print(currentRotation);
     }
 
     void ProcessFiring()
@@ -70,13 +97,13 @@ public class PlayerMovement : MonoBehaviour
             {
             SetGunsActive(true);
             //StartFiringSFX();
-            print("Set guns active true");
+            //print("Set guns active true");
         }
         else
         {
             SetGunsActive(false);
             //StopFiringSFX();
-            print("Set guns active false");
+            //print("Set guns active false");
         }
     }
 
@@ -85,9 +112,9 @@ public class PlayerMovement : MonoBehaviour
         foreach (GameObject gun in guns)
         {
             var emissionModule = gun.GetComponent<ParticleSystem>().emission;
-            print("Get particle system");
+            //print("Get particle system");
             emissionModule.enabled = isActive;
-            print("emissionModule isActive");
+            //print("emissionModule isActive");
         }
     }
 }
