9fa296ac72566389c47b7244ac225879472b8deb
Soren's Coding Land <chaz.com@hotmail.co.uk>
2019-04-16 22:17:28 +0100

    Music Player - Done - Will need improvements when options menu developed.
    I've made a music player that a track can be set to each scene in the build order except for scene 0 that has to be done seperatly!
    It may be that arrays take time to load and simply won't be ready for the first scene.

---
+++ Isolation & Colours project/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip[] levelMusicArray;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Don't destroy on load: " + name);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += Onlevelwasloaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= Onlevelwasloaded;
+    }
+
+    void Onlevelwasloaded (Scene scene, LoadSceneMode mode) // LoadSceneMode mode is needed for the OnLevelwasloaded overload to work. mode is just a name.
+    {
+        // The build index code aligns the audio clips with the scene numbers with exception of element 0/scene 0.
+        AudioClip thisLevelMusic = levelMusicArray[scene.buildIndex];
+        Debug.Log("Playing clip" + levelMusicArray[scene.buildIndex]);
+        // Replace the above with a int count list or array. On level loaded needs to say if scene.buildindex > 2 then thisLevelMusic else play element 1 and 2? 
+        // Fuck this won't work.
+        if (thisLevelMusic)
+        { // If there's some music attached
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true; // May need changing once I have more than one track to test. Could just record me saying track 1, track 2 and a track 3.
+            audioSource.Play();
+        }
+    }
+
+}
