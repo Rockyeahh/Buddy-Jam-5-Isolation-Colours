bdfb5a3d420593bd2d2debf7f68626939dc212c4
Charlie <chaz.com@hotmail.co.uk>
2019-04-24 23:24:11 +0100

    Mirror script
    Most of the mirror script is now done. I'm starting to space out so I will continue another day. I still need to get the blue colour set when needed and the red set when needed rather than just red at the moment.
    I've pushed this commit to GitHub in case my attempts to add in the blue cause it to break somehow. Right now things aren't broken!

---
+++ Isolation & Colours project/Assets/Scripts/Mirror.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour
+{
+    // Add in a inspector message for the bool bellow to tell the designer to only tick this if it needs to be the correct mirror/final mirror in a puzzle room/level.
+    [SerializeField] bool Stuffname = false; // TODO: A better nmame is needed!
+     SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get SpriteRenderer here?
+         spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnParticleCollision(GameObject gameObject) // Maybe because it bounces off, it doesn't Enter and thus OnCollision Enter doesn't get called.
+                                                           // Maybe it needs to be more than discrete, could be continous.
+    {                                           // What about if it's positive or negative/the final mirror in a level/puzzle?
+        // Get Sprite Renderer                  // Get the Sprite Renderer changing colour first. Then work if conditions.
+        // SpriteRenderer set color red
+         spriteRenderer.color = Color.red; // This could be replaced with a sound to reprsent it later and maybe even some kind of particle effect 
+                                          // or light and that glows around the Mirror that resets after a few seconds.
+    }
+
+    // Just some notes: The set color.red stuff above needs to set it as that if it's not the final mirror, we'll call that winMirror.
+    // What can serialize? Can we serialize a tickable box? If box ticked then you are winMirror. By default set it to un-ticked as most will be un-ticked.
+    // 
+
+}
