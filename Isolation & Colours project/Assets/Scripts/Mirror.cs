62b4b8796fac09a00a98b144c6cef41257e7e63a
Charlie <chaz.com@hotmail.co.uk>
2019-04-28 20:58:03 +0100

    New level 1 & the old Level 1 is now Level 2.
    The first level is very simple now as that seems better for learning the simple controls. :)
    Each level it can slightly increase in difficulty from this point onwards.

--- Isolation & Colours project/Assets/Scripts/Mirror.cs
+++ Isolation & Colours project/Assets/Scripts/Mirror.cs
@@ -23,17 +23,13 @@ public class Mirror : MonoBehaviour
 
     void OnParticleCollision(GameObject gameObject)
     {
-        print("colliision");
         if (CurrentMirrorstate == false)
         {
             spriteRenderer.color = Color.red; // This could be replaced with a sound to reprsent it later and maybe even some kind of particle effect 
                                               // or light and that glows around the Mirror that resets after a few seconds.
-            print("Set to Red");
         } else if (CurrentMirrorstate == true)
         {
             spriteRenderer.color = Color.blue;
-            print("Set to Blue");
-            print("Level complete. Load next level or open door/wall to the next level.");
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
